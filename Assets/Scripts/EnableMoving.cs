1eba29a3e479b9dfc95455be0a0ee068fecfc401
isaekrrrr <dannyquinto24@gmail.com>
2021-05-17 04:18:30 -0700

    Initial commit

---
+++ Assets/Scripts/EnableMoving.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableMoving : MonoBehaviour
+{
+    public MovePlatform script;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            script.enabled = true;
+        }
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hello THere");
+    }
+}
+  
+
